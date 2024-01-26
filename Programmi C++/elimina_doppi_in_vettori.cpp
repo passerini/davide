685a89331e1c92621dd6d24e13ebdd2bcef4e238
passerini <andrea.passerini@mediaset.it>
2024-01-26 10:55:05 +0100

    new files

--- Programmi C++/elimina_doppi_in_vettori.cpp
+++ Programmi C++/elimina_doppi_in_vettori.cpp
@@ -1,6 +1,4 @@
-// #include <iostream>
-#include <stdio.h>
-// #define Ntot 10
+#include <iostream>
 #define N 5
 
 int main()
@@ -8,38 +6,30 @@ int main()
 
     int vet1[N];
     int vet2[N];
-    // int vet_totale[Ntot];
 
-    int i, y;
+    int i,y;
 
     for (i = 0; i < N; i++)
     {
-        int numero1;
         printf("inserisci elemento del primo vettore \n");
-        scanf("%d", &numero1);
+        scanf("%d", &vet1[i]);
 
-        vet1[i] = numero1;
-        // vet_totale[i] = numero1;
-
-        int numero2;
         printf("inserisci elemento del secondo vettore \n");
-        scanf("%d", &numero2);
+        scanf("%d", &vet2[i]);
 
-        vet2[i] = numero2;
-        // vet_totale[i+N] = numero2;
     }
 
     for (i = 0; i < N; i++)
     {
         printf("\n Verifico se e' duplicato il numero: ");
-        printf(" %d ", vet1[i]);
+        printf(" %d ", &vet1[i]);
 
         for (y = 0; y < N; y++)
         {
             if (vet1[i] == vet2[y])
             {
                 printf("\n Il seguente numero e' duplicato : ");
-                printf(" %d ", vet2[y]);
+                printf(" %d ", &vet2[y]);
             }
         }
     }
---
+++ Programmi C++/funzioni.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#define N 5
+#define T 10
+
+void inserimento(int arr[], int l);
+int esclusione(int arr1[], int arr2[], int l, int arr3[]);
+void stampa(int arr[], int l);
+
+int main()
+{
+	int arr1[N], arr2[N], arr3[T];
+	inserimento(arr1, N);
+	inserimento(arr2, N);
+	int l = esclusione(arr1, arr2, N, arr3);
+	stampa(arr3, l);
+}
+
+void stampa(int arr[], int l) {
+	for (int i = 0; i < l; i++) {
+		printf("%d\n", arr[i]);
+	}
+}
+
+int esclusione(int arr1[], int arr2[], int l, int arr3[]) {
+	int l2 = 0;
+	for (int i = 0; i < l; i++) {
+		int found = 0;
+		for (int j = 0; j < l; j++) {
+			if (arr1[i] == arr2[j]) {
+				found = 1;
+				break;
+			}
+		}
+		if (!found) {
+			if (l2 == 0) {
+				arr3[l2] = arr1[i];
+				l2++;
+			}
+			else {
+				int found2 = false;
+				for (int j = 0; j < l2; j++) {
+					if (arr1[i] == arr3[j]) {
+						found2 = true;
+						break;
+					}
+				}
+				if (!found2) {
+					arr3[l2] = arr1[i];
+					l2++;
+				}
+			}
+		}
+	}
+	return l2;
+}
+
+void inserimento(int arr[], int l) {
+	for (int i = 0; i < l; i++) {
+		printf("Inserire il valore %d: ", i + 1);
+		scanf("%d", &arr[i]);
+	}
+}
