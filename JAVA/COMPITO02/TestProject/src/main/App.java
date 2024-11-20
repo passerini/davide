dc52ac05a8c9f4e75622d9527c23f88e0cecc694
passerini <andrea@passerini.org>
2024-11-20 13:54:27 +0100

    sync

--- JAVA/COMPITO02/TestProject/src/main/App.java
+++
@@ -1,9 +0,0 @@
-package main;
-import data.Somma;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        Somma somma = new Somma(5, 3);
-        System.out.println("La somma è: " + somma.calcola());
-    }
-}
