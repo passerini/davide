ac3ee4e2f25533bf5282f34d5a4f34ce698d13d0
passerini <andrea@passerini.org>
2024-11-20 14:02:26 +0100

    sync

---
+++ Progetto01/TestApp/src/App.java
@@ -0,0 +1,16 @@
+import datas.Somma;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Hello, World!");
+        int numeroUno = 1;
+        int numeroDue = 2;
+
+
+        Somma somma = new Somma(numeroUno, numeroDue);
+        System.out.println(somma);
+
+    }
+}
---
+++ Progetto01/TestApp/src/datas/Somma.java
@@ -0,0 +1,8 @@
+package datas;
+
+public class Somma {
+
+    public Somma(int num1, int num2) {
+        System.out.println("Somma");
+    }
+}
